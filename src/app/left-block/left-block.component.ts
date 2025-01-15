87e19e209b5b460960db865bf84de5cd8afa24ba
mossoii84 <username@users.noreply.github.com>
2025-01-15 22:39:27 +0100

    commit 3

--- src/app/left-block/left-block.component.ts
+++ src/app/left-block/left-block.component.ts
@@ -3,10 +3,11 @@ import { TaskComponent } from '../tasks-block/task.component';
 import { DateTodayComponent } from '../date-times/date-today/date-today.component';
 import { SelectModule } from 'primeng/select';
 import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-left-block',
-  imports: [TaskComponent, FormsModule, DateTodayComponent, SelectModule],
+  imports: [TaskComponent, FormsModule, DateTodayComponent, SelectModule, RouterLink, RouterLinkActive],
   templateUrl: './left-block.component.html',
   styleUrl: './left-block.component.scss'
 })
