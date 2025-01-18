06375be47fb6c4e63774e248f55c6968e5149339
mossoii84 <username@users.noreply.github.com>
2025-01-18 19:58:30 +0100

    title

--- src/app/files-configuration/icons/icons-list.ts
+++ src/app/files-configuration/icons/icons-list.ts
@@ -15,12 +15,12 @@ export const Iconslist: Icons = {
     dev: '/assets/icons/zadachi/dev.svg',
     logo: '/assets/icons/zadachi/logo16.svg',
     clock:'/assets/icons/zadachi/clock_12.svg',
-    ios:'/assets/icons/ios/ios.svg',
     feature:'/assets/icons/zadachi/Feature.svg',
     icondown:'/assets/icons/raznoe/chevron.down.min.svg',
     topclock:'/assets/icons/menutop/top-clock.svg',
     bug:'/assets/icons/zadachi/bug1.svg',
     category:'/assets/icons/raznoe/category.svg',
     test:'/assets/icons/raznoe/test.svg',
+    ios: '/assets/icons/ios/ios.svg',
 };
 
